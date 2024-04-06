96b11160629a6a5d2d70e35c90530f5a6fa0a5cc
OmkarJadhav98 <160475889+OmkarJadhav98@users.noreply.github.com>
2024-04-06 14:21:36 +0530

    Update Subtraction.java

--- Subtraction/Subtraction.java
+++ Subtraction/Subtraction.java
@@ -22,10 +22,10 @@ public class Subtraction
         char iNo4 = '2';
         int CHAR = (iNo3 - '0') - (iNo4 - '0');
 
-        System.out.println("Addition of integer is : "+INTEGER);
-        System.out.println("Addition of long is : "+LONG);
-        System.out.println("Addition of float is : "+FLOAT);
-        System.out.println("Addition of double is : "+DOUBLE);
-        System.out.println("Addition of character is : "+CHAR);
+        System.out.println("Subtraction of integer is : "+INTEGER);
+        System.out.println("Subtraction of long is : "+LONG);
+        System.out.println("Subtraction of float is : "+FLOAT);
+        System.out.println("Subtraction of double is : "+DOUBLE);
+        System.out.println("Subtraction of character is : "+CHAR);
     }
 }
