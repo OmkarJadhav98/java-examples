4e6b695caa1f67db6a4b1080d5fbcf0a3d39004b
unknown <objadhav28@gmail.com>
2024-04-07 13:21:36 +0530

    Using '!=' this equality operators

--- equality operators/_2.java
+++ equality operators/_2.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class _2 {
+import java.util.Scanner;
+
+public class _2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Please enter a First number:");
+
+        Scanner scanner = new Scanner(System.in);
+        String no1 = scanner.nextLine();
+
+        System.out.println("Please enter a Second number:");
+        String no2 = scanner.nextLine();
+
+        int a = Integer.valueOf(no1);
+        int b = Integer.valueOf(no2);
+
+        if(a != b)
+        {
+            System.out.println("Value of 'a' is not equals to 'b'.");
+        }
+        else
+        {
+            System.out.println("Value of 'a' is exact equals to 'b'.");
+        }
+    }
 }
