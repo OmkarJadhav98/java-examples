a89924b731517bbbeab0ae2a079d7817c6370ea1
unknown <objadhav28@gmail.com>
2024-04-15 14:59:52 +0530

    Using InstanceOf operator

--- InstanceOfOperator/InstanceOf.java
+++ InstanceOfOperator/InstanceOf.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class InstanceOf {
+public class InstanceOf
+{
+    public static void main(String[] args)
+    {
+        String city = "Pune";
+        Integer length = 20;
+        Double temp = 35.5;
+
+        if (city instanceof String)
+        {
+            System.out.println("This type of city is String");
+        }
+        else
+        {
+            System.out.println("This type of city is Not String");
+        }
+
+        if (length instanceof Integer)
+        {
+            System.out.println("This type of length is Integer");
+        }
+        else
+        {
+            System.out.println("This type of length is Not Integer");
+        }
+
+        if (temp instanceof Double)
+        {
+            System.out.println("This type of temp is Double");
+        }
+        else
+        {
+            System.out.println("This type of temp is Not Double");
+        }
+    }
 }
