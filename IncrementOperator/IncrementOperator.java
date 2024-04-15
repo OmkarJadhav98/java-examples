9d6c9ed278d61ce22e78194b058288484540d65d
unknown <objadhav28@gmail.com>
2024-04-15 14:51:43 +0530

    using increment operator

--- IncrementOperator/IncrementOperator.java
+++ IncrementOperator/IncrementOperator.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class IncrementOperator {
+import java.util.Scanner;
+
+public class IncrementOperator
+{
+        /*===================================================================================
+         Pseudo code:
+            1. create a variable and name it as no = 1
+            2. loop no <= 21
+        	3. print no
+        	4. no = no + 1
+        ===================================================================================*/
+        private static Scanner scanner = new Scanner(System.in);
+
+        public static void incrementOperatorPrints(int range)
+        {
+            System.out.println("------------- Increment operator output Start ---------------");
+            int no = 1;
+            while (no <= range) {
+                System.out.println(no);
+                no++; //no = no + 1;
+            }
+            System.out.println("------------- Increment operator output ends here ---------------");
+        }
+        public static void main(String[] args)
+        {
+            System.out.println("Please enter the Number: ");
+            int range = scanner.nextInt();
+            incrementOperatorPrints(range);
+        }
 }
