c3380846e200e1a231c2221dba63bcffea741cec
unknown <objadhav28@gmail.com>
2024-04-15 14:52:57 +0530

    using decrement operator

--- DecrementOperator/src/DecrementOperator.java
+++ DecrementOperator/src/DecrementOperator.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class DecrementOperator {
+import java.util.Scanner;
+public class DecrementOperator
+{
+        private static Scanner scanner = new Scanner(System.in);
+
+        /*==========================================================================
+        Pseudo code:
+            1.  accept range
+            2.  loop range >= 1
+            3.  print range
+            4.  range = range - 1
+        ==========================================================================*/
+        public static void decrementOperatorPrints(int range)
+        {
+            System.out.println("------------- Decrement operator output ---------------");
+
+            while (range >= 1)
+            {
+                System.out.println(range);
+                range--; //range = range - 1;
+            }
+            System.out.println("------------- Decrement operator output ends here ---------------");
+        }
+
+        public static void main(String[] args)
+        {
+            System.out.println("Please enter the range: ");
+            int range = scanner.nextInt();
+            decrementOperatorPrints(range);
+        }
 }
