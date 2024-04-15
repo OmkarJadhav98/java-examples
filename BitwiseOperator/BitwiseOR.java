a5240522c579120fe162f89f215fc5ec528d4535
unknown <objadhav28@gmail.com>
2024-04-15 14:50:02 +0530

    Using '|' operator

--- BitwiseOperator/BitwiseOR.java
+++ BitwiseOperator/BitwiseOR.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class BitwiseOR {
+import  java.util.Scanner;
+public class BitwiseOR
+{
+    static int No1 = 0;
+    static int No2 = 0;
+    public static void main(String[] args)
+    {
+        System.out.println("Please enter first value:");
+        Scanner scanner = new Scanner(System.in);
+
+        No1 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Please enter second value:");
+        No2 = Integer.valueOf(scanner.nextLine());
+
+        //bitwiseOrOperation();
+        int result = No1 | No2;
+        System.out.println("bitwise OR result: " + result);
+    }
 }
