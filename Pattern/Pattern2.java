ba0d4678e262826831f8ac7ea4e2bedb7acc3c40
unknown <objadhav28@gmail.com>
2024-04-21 14:53:44 +0530

    Using While loop

--- Pattern/Pattern2.java
+++ Pattern/Pattern2.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Pattern2 {
+public class Pattern2
+{
+    /*====================================================
+                  *
+                *   *
+              *   *   *
+            *   *   *   *
+          *   *   *   *   *
+        *   *   *   *   *   *
+    ====================================================*/
+    public static void main(String[] args)
+    {
+        int rows = 6;
+
+        int i = 1;
+        int j = 0;
+
+        while(i <= rows)
+        {
+            j = rows - i;
+            while(j > 0)
+            {
+                System.out.print("  ");
+                j--;
+            }
+            int k = 1;
+            while (k <= i)
+            {
+                System.out.print("*   ");
+                k++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
 }
