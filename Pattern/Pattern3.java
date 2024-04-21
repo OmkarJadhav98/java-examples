907ce735061c44eae4199fc7223d500a19983c69
unknown <objadhav28@gmail.com>
2024-04-21 14:54:04 +0530

    Using While loop

--- Pattern/Pattern3.java
+++ Pattern/Pattern3.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Pattern3 {
+public class Pattern3
+{
+    /*====================================================
+
+                                  *
+                                *   *
+                              *   *   *
+                            *   *   *   *
+                          *   *   *   *   *
+                        *   *   *   *   *   *
+                      *   *   *   *   *   *   *
+                     *  *   *   *   *   *   *   *
+                      *   *   *   *   *   *   *
+                        *   *   *   *   *   *
+                          *   *   *   *   *
+                            *   *   *   *
+                              *   *   *
+                                *   *
+                                  *
+
+    ====================================================*/
+    public static void main(String[] args)
+    {
+        int rows = 8;
+
+        int i = 1;
+        int j = 0;
+
+        while(i <= rows)
+        {
+            j = rows - i;
+            while(j > 0)
+            {
+                System.out.print("  ");
+                j--;
+            }
+            int k = 1;
+            while (k <= i)
+            {
+                System.out.print("*   ");
+                k++;
+            }
+            System.out.println();
+            i++;
+        }
+        i = rows - 1;
+        while(i > 0)
+        {
+            j = rows - i;
+            while (j > 0)
+            {
+                System.out.print("  ");
+                j--;
+            }
+            int k = 1;
+            while (k <= i) {
+                System.out.print("*   ");
+                k++;
+            }
+            System.out.println();
+            i--;
+        }
+    }
 }
