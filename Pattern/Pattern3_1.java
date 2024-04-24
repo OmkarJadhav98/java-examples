8bf2102a028de277f521bcbc3189cb7839016fbf
unknown <objadhav28@gmail.com>
2024-04-24 12:10:07 +0530

    Using for loop

--- Pattern/Pattern3_1.java
+++ Pattern/Pattern3_1.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class Pattern3_1 {
+public class Pattern3_1
+{
+    /*====================================================
+
+                                  *
+                                *   *
+                              *   *   *
+                            *   *   *   *
+                          *   *   *   *   *
+                        *   *   *   *   *   *
+                      *   *   *   *   *   *   *
+                     *  *   *   *   *   *   *   *
+                      *   *   *   *   *   *   *
+                        *   *   *   *   *   *
+                          *   *   *   *   *
+                            *   *   *   *
+                              *   *   *
+                                *   *
+                                  *
+
+    ====================================================*/
+    public static void main(String[] args)
+    {
+        int rows = 8;
+
+        int i = 1;
+        int j = 0;
+
+        for(;i <= rows;i++)
+        {
+            j = rows - i;
+            for(;j > 0;j--)
+            {
+                System.out.print("  ");
+            }
+            int k = 1;
+            for(;k <= i;k++)
+            {
+                System.out.print("*   ");
+            }
+            System.out.println();
+        }
+
+        i = rows - 1;
+        for(;i > 0;i--)
+        {
+            j = rows - i;
+            for(;j > 0;j--)
+            {
+                System.out.print("  ");
+            }
+            int k = 1;
+            for(;k <= i;k++)
+            {
+                System.out.print("*   ");
+            }
+
+            System.out.println();
+        }
+    }
 }
