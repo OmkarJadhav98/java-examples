517944f519ee36bdc4e0511349bf2b596491999f
unknown <objadhav28@gmail.com>
2024-04-24 12:08:00 +0530

    Using for loop

---
+++ Pattern/Pattern1_1.java
@@ -0,0 +1,34 @@
+public class Pattern1_1
+{
+    /*====================================================
+                            *
+                        *   *
+                    *   *   *
+                *   *   *   *
+            *   *   *   *   *
+        *   *   *   *   *   *
+    ====================================================*/
+    public static void main(String[] args)
+    {
+        int rows = 6;
+
+        int i = 1;
+        int j = 0;
+
+        for(;i <= rows;i++)
+        {
+            j = rows - i;
+            for(;j > 0;j--)
+            {
+                System.out.print("    ");
+            }
+            int k = 1;
+            for (;k <= i;k++)
+            {
+                System.out.print("*   ");
+            }
+            System.out.println();
+
+        }
+    }
+}
