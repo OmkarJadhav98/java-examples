624abc9084f851552964d3adc327ba42e8b7c0d0
unknown <objadhav28@gmail.com>
2024-04-24 12:10:27 +0530

    Using Do-While loop

--- Pattern/Pattern3_2.java
+++ Pattern/Pattern3_2.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class Pattern3_2 {
+public class Pattern3_2
+{
+    /*====================================================
+
+                                  *
+                                *   *
+                              *   *   *
+                            *   *   *   *
+                          *   *   *   *   *
+                        *   *   *   *   *   *
+                      *   *   *   *   *   *   *
+                     *  *   *   *   *   *   *   *
+                      *   *   *   *   *   *   *
+                        *   *   *   *   *   *
+                          *   *   *   *   *
+                            *   *   *   *
+                              *   *   *
+                                *   *
+                                  *
+
+    ====================================================*/
+    public static void main(String[] args)
+    {
+        int rows = 8;
+
+        int i = 1;
+        int j = 0;
+
+        do
+        {
+            j = rows - i;
+            int k = 1;
+            do
+            {
+                System.out.print("  ");
+                j--;
+            }while(j >= 0);
+
+            do
+            {
+                System.out.print("*   ");
+                k++;
+            }while (k <= i);
+
+            System.out.println();
+            i++;
+        }while(i <= rows);
+
+        i = rows - 1;
+        do
+        {
+            j = rows - i;
+            do
+            {
+                System.out.print("  ");
+                j--;
+            }while (j >= 0);
+
+            int k = 1;
+            do
+            {
+                System.out.print("*   ");
+                k++;
+            }while (k <= i);
+
+            System.out.println();
+            i--;
+        }while(i > 0);
+    }
 }
+
