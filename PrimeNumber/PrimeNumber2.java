a7a571438bf12f565deebf1bb951caa4a488a491
unknown <objadhav28@gmail.com>
2024-04-30 21:28:52 +0530

    check whether the number is Prime or Not by using For loop

--- PrimeNumber/PrimeNumber2.java
+++ PrimeNumber/PrimeNumber2.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class PrimeNumber2 {
+import java.util.Scanner;
+
+public class PrimeNumber2
+{
+    public static boolean isPrime(int num)
+    {
+        if (num <= 1)
+        {
+            return false;
+        }
+        int i = 2;
+        for(; i <= Math.sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check if it's prime: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number))
+        {
+            System.out.println(number + " is a prime number.");
+        }
+        else
+        {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
 }
+
+
