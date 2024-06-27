ba4de28a56484159ceeb9915253ce523627ae66f
Omkar Jadhav <objadhav28@gmail.com>
2024-06-27 14:15:58 +0530

    Using ArrayList

---
+++ LambdaExpression/_2.java
@@ -0,0 +1,17 @@
+/*Write a Java program to iterate through all elements in an array list.*/
+
+import java.util.ArrayList;
+public class _2 {
+    public static void main(String[] args) {
+        // Creae a list and add some colors to the list
+        ArrayList<String> list_Strings = new ArrayList<String>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+
+        list_Strings.forEach(element -> System.out.println(element));
+    }
+}
+
