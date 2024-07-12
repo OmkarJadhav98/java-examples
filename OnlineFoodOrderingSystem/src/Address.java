89aa0a889b93ade0220742b4ecc8508872afc8ff
Omkar Jadhav <objadhav28@gmail.com>
2024-07-12 20:56:47 +0530

    No Commit message

---
+++ OnlineFoodOrderingSystem/src/Address.java
@@ -0,0 +1,17 @@
+class Address {
+    private long id;
+    private String name;
+    private String flatNo;
+    private String buildingName;
+    private String city;
+
+    public Address(long id, String name, String flatNo, String buildingName, String city) {
+        this.id = id;
+        this.name = name;
+        this.flatNo = flatNo;
+        this.buildingName = buildingName;
+        this.city = city;
+    }
+
+    // Getters and setters
+}
